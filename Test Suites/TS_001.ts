d041c8763f389f1feea4e5e9164f719795e5cb1c
akash20182019 <45789225+akash20182019@users.noreply.github.com>
2018-12-27 19:28:08 +0530

    test

---
+++ Test Suites/TS_001.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9c6b1833-333c-46c4-89f0-a81646f5cd28</testSuiteGuid>
+   <testCaseLink>
+      <guid>2467d10a-0b97-43de-a7d8-9ebacf7abff8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
